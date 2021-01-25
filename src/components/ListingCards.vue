e9a1a52fd072de2c59e751fe7c7b0abb54b4fd48
Adam Griffin <adam.griffin@moxiworks.com>
2021-01-24 23:41:55 -0800

    Created ListingCards component

---
+++ src/components/ListingCards.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul id="grid">
+    <li class="grid-item" v-for="listing in listings" :key="listing.listingid">
+      <ListingCard :listing="listing" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import ListingCard from '@/components/ListingCard'
+
+export default {
+  components: { ListingCard },
+
+  props: {
+    listings: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+#grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 1.3rem;
+  margin: 0;
+  padding: 0;
+  overflow-y: auto;
+}
+
+.grid-item {
+  display: block;
+  list-style: none;
+}
+</style>
