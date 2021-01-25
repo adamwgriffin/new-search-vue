d627251584ba340e131daff8336064be94aba145
Adam Griffin <adam.griffin@moxiworks.com>
2021-01-24 23:43:16 -0800

    Added ListingsCards and picklist heading to Picklist

--- src/components/PickList.vue
+++ src/components/PickList.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="picklist">
-    <ul>
-      <li class="listing-card" v-for="listing in listings" :key="listing.listing">
-        {{ listing.location.address }}
-      </li>
-    </ul>
-  </div>  
+    <div id="picklist-heading">
+      SHOWING 1 - {{ listings.length }} OF {{ listings.length }} LISTINGS
+    </div>
+    <ListingCards :listings="listings" />
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import ListingCards from '@/components/ListingCards'
 
 export default {
+  components: { ListingCards },
+
   computed: {
     ...mapState('listingSearch', ['listings'])
   }
@@ -20,7 +22,16 @@ export default {
 
 <style scoped>
 #picklist {
-  width: 60%;
-  padding: .5rem;
+  flex: 1;
+  background: #f6f6f6;
+  padding: .9rem;
+}
+
+#picklist-heading {
+  height: 2rem;
+  color: #212e35;
+  font-family: Muli,Hevetica,Verdana,Arial,sans-serif;
+  font-weight: 700;
+  font-size: 12px;
 }
 </style>
