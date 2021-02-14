6e3440f5b12472e402754ec131de1c568fb724e9
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-13 21:39:07 -0800

    Using pricemin and pricemax with PriceRange component

--- src/components/PickListHeading.vue
+++ src/components/PickListHeading.vue
@@ -4,13 +4,14 @@
       {{ total }} Listings
     </div>
     <div class="filters">
-      <PriceRange />
+      <PriceRange :value="priceRange" @input="setSearchParams($event)" />
     </div>
   </div>
 </template>
 
 <script>
 import PriceRange from './PriceRange'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   components: { PriceRange },
@@ -19,6 +20,22 @@ export default {
     total: {
       type: Number
     }
+  },
+
+  computed: {
+    ...mapState('listingSearch', [
+      'searchParams'
+    ]),
+
+    priceRange() {
+      return { pricemin: this.searchParams.pricemin, pricemax: this.searchParams.pricemax }
+    }
+  },
+
+  methods: {
+    ...mapMutations('listingSearch', [
+      'setSearchParams'
+    ]),
   }
 }
 </script>
