8ea4cb13e1380ef02d8aa46f4a3b3263bc29cf89
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-13 16:50:30 -0800

    Added PriceRange component

---
+++ src/components/PriceRange.vue
@@ -0,0 +1,17 @@
+<template>
+  <MenuButton :label="'Price Range'">
+    <p>Hello, menu world!</p>
+  </MenuButton>
+</template>
+
+<script>
+import MenuButton from './MenuButton'
+
+export default {
+  components: { MenuButton },
+}
+</script>
+
+<style>
+
+</style>
