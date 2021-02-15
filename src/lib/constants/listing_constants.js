e0613bbe8210d35cad22419b5e0ad275f544e66a
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-15 12:08:21 -0800

    Added listing_constants module

---
+++ src/lib/constants/listing_constants.js
@@ -0,0 +1,13 @@
+export const PROPERTY_STATUS_TYPES = {
+  active: 1,
+  pending: 2,
+  coming_soon: 11,
+  temp_off_market: 8,
+  contingent: 3,
+  expired: 5,
+  sale_fail: 6,
+  canceled: 4,
+  other: 10,
+  short_sale: 7,
+  sold: 9
+}
