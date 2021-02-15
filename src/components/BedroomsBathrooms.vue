d81010b265a4fbb86c20be9709e27c5eea818b8a
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-14 21:35:50 -0800

    Created RadioButtonGroup
    Extracted radio buttons from BedroomsBathrooms into re-usable component

--- src/components/BedroomsBathrooms.vue
+++ src/components/BedroomsBathrooms.vue
@@ -2,53 +2,24 @@
   <MenuButton :label="'Bed + bath'">
     <fieldset id="bed_min">
       <legend>Beds</legend>
-      <div class="container">
-        <template v-for="count in countOptions">
-          <input
-            type="radio"
-            name="bed_min"
-            :value="count"
-            :id="`bed_min_${count}`"
-            :key="`bed_min-radio-${count}`"
-            :checked="count === value.bed_min"
-            @change="updateValue('bed_min', count)"
-          >
-          <label :for="`bed_min_${count}`" :key="`bed_min-label-${count}`">
-            {{ countFormatted(count) }}
-          </label>
-        </template>
-      </div>
+      <RadioButtonGroup :name="'bed_min'" :options="countOptions" @input="updateValue('bed_min', $event)" :value="value.bed_min" />
     </fieldset>
     <fieldset id="bath_min">
       <legend>Baths</legend>
-      <div class="container">
-        <template v-for="count in countOptions">
-          <input
-            type="radio"
-            name="bath_min"
-            :value="count"
-            :id="`bath_min_${count}`"
-            :key="`bath_min-radio-${count}`"
-            :checked="count === value.bath_min"
-            @change="updateValue('bath_min', count)"
-          >
-          <label :for="`bath_min_${count}`" :key="`bath_min-label-${count}`">
-            {{ countFormatted(count) }}
-          </label>
-        </template>
-      </div>
+      <RadioButtonGroup :name="'bath_min'" :options="countOptions" @input="updateValue('bath_min', $event)" :value="value.bath_min" />
     </fieldset>
   </MenuButton>
 </template>
 
 <script>
 import MenuButton from '@/components/MenuButton'
+import RadioButtonGroup from '@/components/RadioButtonGroup'
 
 export default {
-  components: { MenuButton },
+  components: { MenuButton, RadioButtonGroup },
 
   props: {
-    countOptions: {
+    countArr: {
       type: Array,
       default: () => [0, 1, 2, 3, 4, 5, 6]
     },
@@ -57,6 +28,12 @@ export default {
       type: Object,
       default: () => ({ bed_min: 0, bath_min: 0 })
     }
+  },
+
+  computed: {
+    countOptions() {
+      return this.countArr.map(c => ({ label: this.countFormatted(c), value: c }) )
+    },
   },
 
   methods: {
@@ -84,36 +61,5 @@ legend {
   font-weight: bold;
   font-size: 18px;
   margin-bottom: .5rem;
-}
-
-.container {
-  display: flex;
-  border-radius: 4px;
-  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.04), 0px 2px 2px rgba(0, 0, 0, 0.02), 0px 1px 3px rgba(0, 0, 0, 0.06);
-}
-
-label {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  width: 56px;
-  height: 48px;
-  font-size: 14px;
-  border-left: 1px solid #e7e7e7;
-}
-
-label:hover {
-  background: #F6F6F6;
-}
-
-input {
-  position: absolute;
-  opacity: 0;
-  margin: 0;
-}
-
-input[type=radio]:checked + label{
-  background: rgba(4, 87, 175, 0.24);
 }
 </style>
---
+++ src/components/RadioButtonGroup.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <template v-for="option in options">
+      <input
+        type="radio"
+        :name="name"
+        :value="option.value"
+        :id="`${name}_${option.value}`"
+        :key="`${name}-radio-${option.value}`"
+        :checked="option.value === checkedOption"
+        @change="updateValue(option.value)"
+      >
+      <label :for="`${name}_${option.value}`" :key="`${name}-label-${option.value}`">
+        {{ option.label }}
+      </label>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    },
+
+    value: {
+      default: 0
+    },
+
+    options: {
+      type: Array
+    }
+  },
+
+  computed: {
+    checkedOption() { return this.value }
+  },
+
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  border-radius: 4px;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.04), 0px 2px 2px rgba(0, 0, 0, 0.02), 0px 1px 3px rgba(0, 0, 0, 0.06);
+}
+
+label {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 56px;
+  height: 48px;
+  font-size: 14px;
+  border-left: 1px solid #e7e7e7;
+}
+
+label:hover {
+  background: #F6F6F6;
+}
+
+input {
+  position: absolute;
+  opacity: 0;
+  margin: 0;
+}
+
+input[type=radio]:checked + label{
+  background: rgba(4, 87, 175, 0.24);
+}
+</style>
