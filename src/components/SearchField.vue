43229a51dfddf2b1ee128e5c7bd5f8dc62822f57
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-14 18:18:13 -0800

    Putting fix for AutoComplete dropdown back in place
    Making the search field icon an actual submit button seems to have brought back this bug where the form is submitted when you hit the enter key, which causes the search to be performed twice. I think it was happening previously because it was triggering a click even on the search button I had. When I replaced that button with an icon the issue went away. Now that it's a button again the bug is back. A better long-term solution will be to create our own dropdown instead of using Google's.

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -83,11 +83,32 @@ export default {
       )
     },
 
+    /* if any of the dom nodes for .pac-container are visible, it means that the dropdown is open. there seems to be no
+    way to check this programmatically with the api, so we are forced to resort to this instead. this solution is based
+    on: https://stackoverflow.com/a/33357188/1536368 */
+    autocompleteDropdownOpen() {
+      return Array.from(document.getElementsByClassName('pac-container'))
+        .some(node => node.offsetParent !== null)
+    },
+
+    /* if the user selects an autocomplete dropdown item with the keyboard using the enter key, it will submit the form,
+    as well as cause the autocomplete to trigger a "place_changed" event. this causes the the autocomplete value that
+    was selected, as well as the partial text that was typed into the input, to be searched one after the other. */
+
     handleEnter(e) {
+      /* if you open the dropdown with the arrow keys, select a value with the keyboard, then close the dropdown with
+      the escape key, you can get some weird behavior after hitting enter. for some reason it will trigger the search
+      button's click hander. location_search_field will not be updated yet in the store, so it will not search the value
+      that was entered into the field. calling inputChanged makes sure that location_search_field gets updated with the
+      latest value in the input. */
       this.$emit('inputChanged', e.target.value)
-      // TODO: create a different event for this, so there's no confusion about what's happening here. also add comments
-      // to eplain why we're doing it.
-      this.$emit('searchButtonClicked')
+      /* calling preventDefault() stops the form submission event, so that only the "place_changed" event triggers the
+      search. we don't want the form submission event in this scenario because calling autocomplete.getPlace() will give
+      us the geometry for the selection so we don't have to look it up later. in some situations the dropdown may be
+      open but nothing will have been selected when the enter event is triggered. in that case there will be no geometry
+      returned from autocomplete.getPlace(), which we deal with in our "autocompletePlaceChanged" handler that actually
+      does the search */
+      if (this.autocompleteDropdownOpen()) e.preventDefault()
     },
 
     initAutoComplete() {
