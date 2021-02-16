24cc340a49ca5e9e298e41e3b83f1f0970f95566
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-15 16:37:27 -0800

    Created MoreFilters component

---
+++ src/components/MoreFilters.vue
@@ -0,0 +1,45 @@
+<template>
+  <MenuButton label="Filters">
+    <ListingStatus :params="listingStatusParams" @change="updateValue($event)"  />
+  </MenuButton>
+</template>
+
+<script>
+import pick from 'lodash/pick'
+import MenuButton from '@/components/MenuButton'
+import ListingStatus from '@/components/ListingStatus'
+
+export default {
+  components: { MenuButton, ListingStatus },
+
+  /* this is for use with v-model. when used with custom components, the default for v-model is to pass state changes
+  down to a prop named "value" and listen for "input" events from the component to propgate it's changes up to the
+  state. you can override these defaults by setting "prop" and/or "event" in this "model" property. here we're changing
+  the default prop, "value", to be called "searchParams", because the name makes more sense for this component. we are
+  overriding the default "input" event to instead be "change" since that's what we listen to on native checkboxes and
+  radio buttons. */
+  model: {
+    prop: 'searchParams',
+    event: 'change'
+  },
+
+  props: {
+    searchParams: Object
+  },
+
+  computed: {
+    listingStatusParams() {
+      return pick(this.searchParams, ['status', 'ex_pend', 'ex_cs'])
+    }
+  },
+
+  methods: {
+    updateValue(e) {
+      this.$emit('change', e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
