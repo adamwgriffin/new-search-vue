e4e06bd8cde6030edecd93ce556e46d592aee548
Adam Griffin <adam.griffin@moxiworks.com>
2021-02-20 12:23:39 -0800

    Created SortMenu component

---
+++ src/components/SortMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sort-menu">
+    Sort: Distance ▾
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.sort-menu {
+  cursor: pointer;
+}
+</style>
