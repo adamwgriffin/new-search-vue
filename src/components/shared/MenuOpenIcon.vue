2bf1e6f02dec75e27a1eb2540c6d2434f34fe373
Adam Griffin <adam.griffin@moxiworks.com>
2021-03-14 16:23:07 -0700

    Created MenuOpenIcon component

---
+++ src/components/shared/MenuOpenIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="menu-open-icon" :class="menuOpenClasses" role="presentation" >▾</span>
+</template>
+
+<script>
+export default {
+  props: {
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    menuOpenClasses() {
+      return { open: this.open }
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.menu-open-icon {
+  display: inline-block;
+  transform: rotate(0deg);
+  transition: transform 150ms linear;
+}
+
+.menu-open-icon.open {
+  transform: rotate(180deg);
+  transition: transform 150ms linear;
+}
+</style>
