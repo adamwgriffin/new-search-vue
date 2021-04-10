c325bf2233d258ccb1a964bb774a4782d9f77c91
Adam Griffin <adam.griffin@moxiworks.com>
2021-04-10 16:54:10 -0700

    Added level prop to Legend
    So it can function like h1, h2 elements

--- src/components/shared/Legend.vue
+++ src/components/shared/Legend.vue
@@ -1,11 +1,36 @@
 <template>
-  <legend><slot></slot></legend>
+  <legend :class="legendClasses"><slot></slot></legend>
 </template>
+
+<script>
+export default {
+  props: {
+    level: {
+      type: Number,
+      default: 1
+    }
+  },
+
+  computed: {
+    legendClasses() {
+      return { [`level-${this.level}`]: true }
+    }
+  }
+}
+</script>
 
 <style scoped>
 legend {
+  margin-bottom: .6rem;
+}
+
+.level-1 {
   font-weight: 700;
   font-size: 18px;
-  margin-bottom: .6rem;
+}
+
+.level-2 {
+  font-weight: 600;
+  font-size: 14px;
 }
 </style>
