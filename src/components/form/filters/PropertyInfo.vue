62181c00653f521f4ec1c6da14b08ec9b5d733eb
Adam Griffin <adam.griffin@moxiworks.com>
2021-04-10 17:20:38 -0700

    Created PropertyInfo component
    It will be used to contain all property info filters

---
+++ src/components/form/filters/PropertyInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <Fieldset>
+    <Legend>Property Info</Legend>
+    <SquareFeet />
+  </Fieldset>
+</template>
+
+<script>
+import Fieldset from '@/components/shared/Fieldset'
+import Legend from '@/components/shared/Legend'
+import SquareFeet from '@/components/form/filters/SquareFeet'
+
+export default {
+  components: { Fieldset, Legend, SquareFeet },
+}
+</script>
+
+<style scoped>
+
+</style>
