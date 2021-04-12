5590aee651ae1a7f3de96170778ac47b80c490ac
Adam Griffin <adam.griffin@moxiworks.com>
2021-04-11 21:42:09 -0700

    No need to convert numbers to strings here

--- src/lib/helpers/filters_helpers.js
+++ src/lib/helpers/filters_helpers.js
@@ -1,7 +1,5 @@
 import range from 'lodash/range'
 
 export const numericOptions = (numericOptionsRanges) => {
-  return numericOptionsRanges
-    .reduce((numbers, numberRange) => numbers.concat(range(...numberRange)), [])
-    .map(numbers => numbers.toString())
+  return numericOptionsRanges.reduce((numbers, numberRange) => numbers.concat(range(...numberRange)), [])
 }
