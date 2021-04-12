fab234f21f105f7997638ebbf4a250d5eca15f4a
Adam Griffin <adam.griffin@moxiworks.com>
2021-04-11 21:20:35 -0700

    Just maintain WEBSITES_SEARCH_PARAMS for now
    Auditing and separating out params for each app will be easier once we've figured out what app has what params in common. There are too many to figure it out at this point.

--- src/lib/constants/search_param_constants.js
+++ src/lib/constants/search_param_constants.js
@@ -1,6 +1,6 @@
 import { PROPERTY_STATUS_TYPE_IDS } from '@/lib/constants/listing_constants'
 
-export const CORE_PARAMS = {
+export const WEBSITES_SEARCH_PARAMS = {
   location_search_field: '',
   agent_uuid: 'f74a3f6d-aeda-4daa-835e-029386152405',
   pgsize: 20,
@@ -14,25 +14,9 @@ export const CORE_PARAMS = {
   openhouse_in_person: false,
   // a.k.a., "property type"
   ptype: [1, 2, 9],
-}
-
-// params used exclusively for IDX (public) search, e.g., Websites
-export const IDX_PARAMS = {
   status: 'active',
   ex_pend: true,
-  ex_cs: true
-}
-
-// params used exclusively in CMA
-export const CMA_PARAMS = {
-  pstatus: PROPERTY_STATUS_TYPE_IDS,
-  bath_max: null,
-  bed_max: null
-}
-
-export const WEBSITES_SEARCH_PARAMS = {
-  ...CORE_PARAMS,
-  ...IDX_PARAMS,
+  ex_cs: true,
 }
 
 // these are only the search params that are needed for the "(More) Filters" part of the Websites form
