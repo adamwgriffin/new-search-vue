e6d9f9a853f0d747b043e8ed1da1d83c40acbdb5
Adam Griffin <adam.griffin@moxiworks.com>
2021-06-01 21:39:55 -0700

    Added new icons

---
+++ src/components/shared/icons/CaretDownIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    width="10"
+    viewBox="0 0 12 6"
+    fill="#212e35"
+    class="caret-down"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <polygon points="0 0 6 6 12 0"></polygon>
+  </svg>
+</template>
---
+++ src/components/shared/icons/SortAscendingIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    fill="#212e35"
+    height="12px"
+    viewBox="0 0 402 314"
+    xmlns="http://www.w3.org/2000/svg"
+    class="sort-direction-icon"
+  >
+    <path
+      d="M402,96.0283768 L305.426821,0 L201,96.0283768 L224.516556,119.406898 L279.31457,80.5676758 L279.31457,308.486198 C279.31457,311.507761 281.799338,314 284.860927,314 L318.139073,314 L318.139073,80.5456206 L365.083444,127.214441 L388.6,103.83592 L402,96.0283768 Z"
+    ></path>
+    <rect id="Rectangle" x="0" y="119" width="126" height="33"></rect>
+    <rect id="Rectangle" x="0" y="40" width="126" height="33"></rect>
+    <rect id="Rectangle" x="0" y="202" width="223" height="33"></rect>
+    <rect id="Rectangle" x="0" y="281" width="223" height="33"></rect>
+  </svg>
+</template>
