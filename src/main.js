3b8d31e3bf82a6ba480fe43f346058534030aa4d
Adam Griffin <adam.griffin@moxiworks.com>
2021-08-09 23:11:27 -0700

    Formatting

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import i18n from './plugins/i18n';
+import i18n from './plugins/i18n'
 
 Vue.config.productionTip = false
 
