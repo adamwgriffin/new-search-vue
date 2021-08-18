26193e08516651f777977562a5c58b10ccc807ee
Adam Griffin <adam.griffin@moxiworks.com>
2021-08-18 10:44:47 -0700

    Using ContainedButton for BoundaryControl

--- src/components/map/BoundaryControl.vue
+++ src/components/map/BoundaryControl.vue
@@ -1,8 +1,16 @@
 <template>
-  <button id="boundary-control" @click="$emit('click')">
+  <ContainedButton id="boundary-control" @click="$emit('click')">
     Remove Boundary
-  </button>
+  </ContainedButton>
 </template>
+
+<script>
+import ContainedButton from '@/components/shared/ContainedButton'
+
+export default {
+  components: { ContainedButton }  
+}
+</script>
 
 <style scoped>
 #boundary-control {
@@ -11,20 +19,6 @@
   transform: translateX(-50%);
   bottom: 24px;
   z-index: 1;
-  cursor: pointer;
-  border: none;
-  box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
-  height: 40px;
-  border-radius: 6px;
-  padding: 0 .8rem;
   margin: 0 0 5px 8px;
-  background: #fefefe;
-  font-size: 14px;
-  font-family: inherit;
-  color: inherit;
-}
-
-#boundary-control:hover {
-  background: #f6f6f6;
 }
 </style>
