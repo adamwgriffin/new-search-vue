f5146cb65844b321811addfad4286a5469032c23
Adam Griffin <adam.griffin@moxiworks.com>
2021-08-18 10:39:53 -0700

    Added TextButton

---
+++ src/components/shared/TextButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="text-button" :style="theme" @click="$emit('click', $event);">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-button {
+  --text-button-color: inherit;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  border: none;
+  border-radius: 6px;
+  height: 40px;
+  padding: 0 .8rem;
+  font-size: 14px;
+  font-family: inherit;
+  background: none;
+  color: var(--text-button-color);
+}
+
+.text-button:hover {
+  background: #f6f6f6;
+}
+</style>
