2519b9fc05854ed0910f8e4cc5479bd48c9ebc71
Adam Griffin <adam.griffin@moxiworks.com>
2021-08-18 21:53:35 -0700

    Renamed store modules
    Just to make them easier to differentiate from components with the same name

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,6 +1,6 @@
-import env from '@/store/modules/env'
-import listingMap from '@/store/modules/listingMap'
-import listingSearch from '@/store/modules/listingSearch'
+import env from '@/store/modules/env_store'
+import listingMap from '@/store/modules/listing_map_store'
+import listingSearch from '@/store/modules/listing_search_store'
 
 export default {
   env,
