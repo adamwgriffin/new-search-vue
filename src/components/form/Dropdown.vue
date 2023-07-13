eb8d42bd4b5c66414905e2737d3656f07e1eec7b
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-07-12 23:17:14 -0700

    Added Dropdown component

---
+++ src/components/form/Dropdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul v-show="open" class="listbox-menu" role="listbox" tabindex="-1">
+    <slot></slot>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      open: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .listbox-menu {
+    position: absolute;
+    z-index: 1;
+    overflow-x: hidden;
+    list-style: none;
+    background-color: white;
+    width: 100%;
+    box-shadow: 0px 3px 3px 0 rgba(0, 0, 0, 0.4);
+    margin: 0;
+    padding: 0;
+  }
+</style>
